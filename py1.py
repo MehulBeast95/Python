f9036f3ef0e2e4ea8922a06a08064ba6295b9a15
Mehul Bisht <mehulbeast95@gmail.com>
2024-01-23 22:09:59 +0530

    Initial Commit

--- py1.py
+++ py1.py
@@ -1,5 +1,39 @@
-import random
-if random.randint(1,6)==1:
-    print("Hello World")
-else:
-    print("Bye World")
+z= int(input("Enter First no: \n"))
+x=int(input("Enter Next no: \n"))
+a=z+x
+b=z-x
+c=z*x
+d=z%x
+e=z//x
+f=z/x
+g=z**x
+h=z==x
+i=x>>2
+j=z&x
+k=z|x
+l=z<<2
+print(z==x)
+print(z!=x)
+print(z>=x)
+print(z<=x)
+print(z>x)
+print(z<x)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+m=True
+n=False
+print(m and n)
+print(m is not n)
+print(not n)
+print(not m)
+print(m or n)
