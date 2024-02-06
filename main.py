5a5ff0b893136e4714859f1784596cc6a11d2eeb
Mehul Bisht <mehulbeast95@gmail.com>
2024-02-06 09:13:43 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 stk = []
+
+
 def push():
     while True:
         x = input("Enter name: ")
@@ -7,6 +9,8 @@ def push():
         ch = input("Enter more ? :  ")
         if ch == "no":
             break
+
+
 def pop():
     if not stk:
         print("Underflow")
@@ -14,10 +18,14 @@ def pop():
         z = stk.pop()
         print("deleted element: ", z)
         display()
+
+
 def display():
     a = len(stk - 1)
     for i in range(a - 1, -1, -1):
         print(stk[i])
+
+
 while True:
     print("1.Add\n2.Delete \n3.Display \n4.Exit \n")
     c = int(input("Enter choice(1-4): "))
